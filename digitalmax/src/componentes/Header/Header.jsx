a572d2ee46f0ff5e4db026c679d7dc5a1dbdffe2
Marlon.C <USTR025024@ugb.edu.sv>
2025-10-16 09:37:27 -0600

    FIX ajuste de diseño

--- digitalmax/src/componentes/Header/Header.jsx
+++ digitalmax/src/componentes/Header/Header.jsx
@@ -4,10 +4,16 @@ import logo from '../../assets/img/Logo digitalmax.png'
 import shop from '../../assets/img/icon shop.png'
 import user from '../../assets/img/icon user.png'
 import search from '../../assets/img/icon search.png'
+import engranaje from '../../assets/img/Icon_engranaje.png'
+import { useState } from 'react'
 
 
 
 function Header(){
+  const [menu, setMenu] = useState (false)
+  const cambioMenu = () =>{
+    setMenu(!menu)
+  }
     return(
         <header className='headerComponent'>
             <div>
@@ -23,8 +29,15 @@ function Header(){
                   <div>
                     <img src= {shop} alt="" />
                     <p>$0.00</p>
+                   
                   </div>
-                    <img src= {user} alt="" />
+                    <img src= {user} alt=""  onClick={cambioMenu}/>
+                     <ul className={`contentMenuHover ${menu ? 'contentMenuHoverActive': ''}`}>
+                      <a href=""><li>Registrate</li></a> <hr/>
+                      <a href=""><li>Iniciar Sesión</li></a>
+                    </ul>
+                    <img src= {engranaje} alt="" />
+
             </div>
             
         </header>
--- digitalmax/src/main.jsx
+++ digitalmax/src/main.jsx
@@ -4,9 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import FormLogin from './Paginas/FormLogin.jsx'
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    
     <App />
   </StrictMode>,
 )
