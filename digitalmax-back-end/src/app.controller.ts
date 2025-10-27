5b0853d43469eae0f1b6587b53d443aeed884a93
Alexis Castillo <USTR007724@ugb.edu.sv>
2025-10-26 18:25:57 -0600

    Feat: CRUD completo para productos listo

---
+++ digitalmax-back-end/src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()//Decorador
+export class AppController {
+  constructor(private readonly appService: AppService) {}//Constructor de la clase
+
+  @Get()
+  byeWorld(): string {
+    return this.appService.getHello();//this objeto interno de la clase
+  }
+
+}
--- digitalmax-back-end/src/app.module.ts
+++ digitalmax-back-end/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
+import { ProductosModule } from './productos/productos.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 
 @Module({
-  imports: [UserModule],
-
+  imports: [ 
+    
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI_Alexis'),
+      }),
+      inject: [ConfigService],
+    }),
+    ProductosModule],
+  controllers: [AppController],
+  providers: [AppService],
+    //UserModule, ProductosModule],
 })
 export class AppModule {}
