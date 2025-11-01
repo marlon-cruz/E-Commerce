cf301165497597e6f1b00c6625f6106e000952ae
Alexis Castillo <USTR007724@ugb.edu.sv>
2025-10-31 20:59:13 -0600

    Fix: Vista detallada de productos actualizada, ahora esta igual al diseño

--- digitalmax/src/componentes/ReseñaProductDetalles/ReseñaProductDetalles.jsx
+++ digitalmax/src/componentes/ReseñaProductDetalles/ReseñaProductDetalles.jsx
@@ -3,12 +3,14 @@ import '../ReseñaProductDetalles/ReseñaProductDetalles.css'
 function ReseñaProductDetalles(props){
     return(
         <div className="reseñaUser">
-            <div>
+            <h3>{props.titulo}</h3>
+            <div className="infoUserReseña">
+                <StardCalificacion tamaño = {props.calificacion}/>  
                 <h4>{props.nombre}</h4>
-                <StardCalificacion tamaño = {props.calificacion}/>
             </div>
-                
+            <span>
                 <p>{props.reseña}</p>
+            </span>
         </div>
         
         
