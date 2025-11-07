4826383bac3c462d2d6d81e9d70e63077b133c1a
Marlon.C <USTR025024@ugb.edu.sv>
2025-11-07 07:06:28 -0600

    FIX: Boton de cantidad, aumenta la cantidad mientras exista stock

--- digitalmax/src/componentes/ButtonCantProduct/ButtonCantProduct.jsx
+++ digitalmax/src/componentes/ButtonCantProduct/ButtonCantProduct.jsx
@@ -1,11 +1,28 @@
 
 import './ButtonCantProduct.css'
+import { useState } from 'react';
+
 function ButtonDetallesProduct(prop){
+    const [countProduct, setCountProduct] = useState(1);
+
+    function conteo (signo){
+        if(signo === "+"){
+            if(countProduct < prop.stock){
+            setCountProduct(countProduct + 1)
+            }
+        }else{
+            if(countProduct <= 1){
+                return
+            }
+            setCountProduct(countProduct - 1)
+        }
+    }
+
     return(
         <div className="contentButtonDetalles">
-                <span>-</span>
-                <div>1</div>
-                <span>+</span>
+                <span onClick={() => {conteo("-")}}>-</span>
+                <div>{countProduct}</div>
+                <span onClick={() => {conteo("+")}}>+</span>
         </div>
     );
 }
