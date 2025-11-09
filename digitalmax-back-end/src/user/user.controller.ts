7ec109c3982deaee0ba40069f1dc69984a980e1a
Marlon.C <USTR025024@ugb.edu.sv>
2025-11-08 21:06:06 -0600

    controladores de carrito de compra listo

--- digitalmax-back-end/src/user/user.controller.ts
+++ digitalmax-back-end/src/user/user.controller.ts
@@ -2,7 +2,10 @@ import { Controller, Get, Post, Delete,Put, Body,Param, ConflictException,HttpCo
 import { UserService } from './user.service';
 import { CreateUserDTO } from 'src/dto/createUser.dto'
 import { UpdateUserDTO } from 'src/dto/updateUser.dto';
+import { carrito } from 'src/schemas/carrito.schema';
 
+import { actualizaCarrito } from 'src/dto/updateUser.dto';
+import { CreateCarrito } from 'src/dto/createUser.dto';
 @Controller('user')
 export class UserController {
     constructor(private userService: UserService){}
@@ -28,6 +31,7 @@ export class UserController {
         }
         
     }
+   
 
     @Post()
     create(@Body()CreateUser: CreateUserDTO){
@@ -60,6 +64,46 @@ export class UserController {
        }
         return consulta;
        
+    }
+//Comienza el controlador de carrito de compra
+
+ @Post(':id/carrito')
+    createitemCarrito(@Param('id') id:string  ,@Body()Createitem: CreateCarrito){
+         try {
+            return  this.userService.createitemCar(id,Createitem);
+        } catch (error) {
+          if(error.code = "11000"){
+            throw new ConflictException("El usuario ya existe")
+          }
+          throw error;
+        }
+    }
+
+     @Get(':id/carrito')
+    async findAllCar(@Param('id') id:string){
+        return this.userService.findOneCarrito(id)
+    }
+    
+    @Put(':id/carrito/:idProduct')
+    @HttpCode(200)
+    async updatecarrito(@Param('id') id:string, @Param('idProduct') idProduct:string,@Body() carrito:actualizaCarrito){
+    const consulta = await this.userService.actualizarItemCarritoUser(id,idProduct,carrito)
+       if(!consulta){
+        throw new ConflictException("El usuario que intenta editar no existe " + consulta);
+       }
+        return consulta;
+       
+    }
+    @Delete(':iduser/carrito/:idproduct')
+    @HttpCode(204)
+    async deleteItemCarrito(@Param('iduser')iduser:string, @Param('idproduct') idProduct:string){
+        
+        const consult = await this.userService.eliminarItemCarritoUser(iduser,idProduct);            
+        if(!consult){
+            throw new ConflictException("El item de carrito que intenta eliminar no existe " + consult )
+        }
+        return consult;
+        
     }
 }   
 
--- digitalmax-back-end/src/user/user.service.ts
+++ digitalmax-back-end/src/user/user.service.ts
@@ -3,35 +3,104 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UpdateUserDTO } from 'src/dto/updateUser.dto';
 import { CreateUserDTO } from 'src/dto/createUser.dto';
-
+import { actualizaCarrito } from 'src/dto/updateUser.dto';
+import { CreateCarrito } from 'src/dto/createUser.dto';
 import { User } from 'src/schemas/user.schema';
+
 
 @Injectable()
 export class UserService {
-    constructor(@InjectModel(User.name) private userModel: Model<User>){}
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-    finALL(){
-       return this.userModel.find();
-    }
+  finALL() {
+    return this.userModel.find();
+  }
 
-    async create(createUser: CreateUserDTO){
-        this.userModel.create();
-        const newUser = new this.userModel(createUser)
+  async create(createUser: CreateUserDTO) {
+    this.userModel.create();
+    const newUser = new this.userModel(createUser);
     return newUser.save();
+  }
+
+  async findOne(correouser: string) {
+    return await this.userModel.findOne({ correo: correouser });
+  }
+
+  async eliminarUser(id: string) {
+    return this.userModel.findByIdAndDelete(id);
+  }
+  async actualizarUser(id: string, user: UpdateUserDTO) {
+    return this.userModel.findByIdAndUpdate(id, user);
+  }
+
+  //
+  //Comienza la configuracion del controlador para el carrito de compra del usuario
+  //
+
+  async findOneCarrito(id: string) {
+    try {
+      const carritoCompra = await this.userModel
+        .find({ _id: id })
+        .select('carrito -_id')
+        .exec();
+      return carritoCompra;
+    } catch (error) {
+      throw 'Carrito no encontrado ' + error;
+    }
+  }
+
+  async createitemCar(idUser:string ,carrito:CreateCarrito){
+    return this.userModel.findByIdAndUpdate(
+        idUser,
+        {
+            $push: {
+                carrito:{
+                    idProducto: carrito.idProducto,
+                    cantSelect: carrito.cantSelect
+                },
+            },
+        },
+        {new:true}
+    )
+
+  }
+
+  async eliminarItemCarritoUser(iduser: string, idProduct: string) {
+    return this.userModel.findByIdAndUpdate(
+        iduser,
+        {
+            $pull:{
+                carrito: {_id:idProduct},
+            },
+
+        },
         
-    }
+        {new:true}
+    
+  );
+  }
 
-    async findOne(correouser:string){
-        return await this.userModel.findOne({ correo: correouser});
-    }
+  async actualizarItemCarritoUser(
+    iduser: string,
+    idProduct: string,
+    carrito: actualizaCarrito,
+  ) {
 
+       return await  this.userModel.findByIdAndUpdate(
+        {_id:iduser},
+        {
+            $set:{
+            'carrito.$[elem].idProducto': carrito.idProducto,
+            'carrito.$[elem].cantSelect': carrito.cantSelect
+            }
+        },
+        {
+            arrayFilters:[{'elem._id': idProduct}],
+            new:true
+        }
 
-    async eliminarUser(id:string){
-        return this.userModel.findByIdAndDelete(id);
-    }
-    async actualizarUser(id:string,user:UpdateUserDTO){
-        return this.userModel.findByIdAndUpdate(id, user);
-    }
-
+       ).exec();
+       
+    }  
 }
 
